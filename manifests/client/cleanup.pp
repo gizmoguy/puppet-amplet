a59185272ff3595e77c2ca24191c03d584f0298d
Brad Cowie <brad@gizmoguy.net.nz>
2014-06-05 16:05:21 +1200

    Fix alignment / whitespace issue.

--- manifests/client/cleanup.pp
+++ manifests/client/cleanup.pp
@@ -1,43 +1,43 @@
 class amplet::client::cleanup {
 
   file { '/etc/amplet2/client.conf':
-    ensure  => absent,
-    path    => '/etc/amplet2/client.conf',
+    ensure => absent,
+    path   => '/etc/amplet2/client.conf',
   }
 
   file { '/etc/amplet2/nametable':
-    ensure  => absent,
-    path    => '/etc/amplet2/nametable',
+    ensure => absent,
+    path   => '/etc/amplet2/nametable',
   }
 
   file { '/etc/amplet2/schedule.d/example.schedule':
-    ensure  => absent,
-    path    => '/etc/amplet2/schedule.d/example.schedule',
+    ensure => absent,
+    path   => '/etc/amplet2/schedule.d/example.schedule',
   }
 
   file { '/etc/amplet2/schedule.d/schedule':
-    ensure  => absent,
-    path    => '/etc/amplet2/schedule.d/schedule',
+    ensure => absent,
+    path   => '/etc/amplet2/schedule.d/schedule',
   }
 
   file { '/etc/amplet2/schedule.d':
-    ensure  => absent,
-    path    => '/etc/amplet2/schedule.d',
+    ensure => absent,
+    path   => '/etc/amplet2/schedule.d',
   }
 
   file { '/etc/amplet2/keys/cacert.pem':
-    ensure  => absent,
-    path    => '/etc/amplet2/keys/cacert.pem',
+    ensure => absent,
+    path   => '/etc/amplet2/keys/cacert.pem',
   }
 
   file { '/etc/amplet2/keys/cert.pem':
-    ensure  => absent,
-    path    => '/etc/amplet2/keys/cert.pem',
+    ensure => absent,
+    path   => '/etc/amplet2/keys/cert.pem',
   }
 
   file { '/etc/amplet2/keys/key.pem':
-    ensure  => absent,
-    path    => '/etc/amplet2/keys/key.pem',
+    ensure => absent,
+    path   => '/etc/amplet2/keys/key.pem',
   }
 
 }
