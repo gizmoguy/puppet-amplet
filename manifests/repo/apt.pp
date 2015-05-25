f54d691ca431b812a277c7cd6cc9796f753b66cd
Brad Cowie <brad@gizmoguy.net.nz>
2015-05-26 09:51:12 +1200

    Allow repo URL to be overriden by user.

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -1,12 +1,16 @@
 # requires
 #   puppetlabs-apt
-class amplet::repo::apt {
+class amplet::repo::apt (
+  $repo = 'http://amp.wand.net.nz/debian/',
+) {
+
+  validate_string($repo)
 
   Class['amplet::repo::apt'] -> Package<| title == 'amplet2-client' |>
   Class['amplet::repo::apt'] -> Package<| title == 'amplet2-server' |>
 
   apt::source { 'amplet-repo':
-    location    => 'http://amp.wand.net.nz/debian/',
+    location    => $repo,
     release     => $::lsbdistcodename,
     repos       => 'main',
     include_src => false,
