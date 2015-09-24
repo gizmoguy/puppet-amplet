8d9176cf50bd4a1f7490cc8166ad43fa3a66d793
Brad Cowie <brad@gizmoguy.net.nz>
2015-09-24 14:06:59 +1200

    Execute apt-get update when we specify ensure=>latest for amplet package.

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -3,11 +3,17 @@ class amplet::client::install {
   $package_ensure   = $amplet::client::package_ensure
   $package_provider = $amplet::client::package_provider
 
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update"
+  }
+
   package { 'amplet2-client':
     ensure   => $package_ensure,
     name     => 'amplet2-client',
     provider => $package_provider,
     notify   => Class['amplet::client::service'],
   }
+
+  Exec["apt-update"] -> Package <| ensure == latest |>
 
 }
